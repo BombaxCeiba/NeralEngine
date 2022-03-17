ec2837657c73c46604bfdba2506a884e87040c5b
Dusk_NM02 <Dusk.11th@outlook.com>
2022-03-17 21:27:38 +0800

    refactor: 规范代码
    更正了宏格式，修改了部分函数名，为GetChar函数添加了noexcept

--- NeralEngine/Common/include/StaticString.hpp
+++ NeralEngine/Common/include/StaticString.hpp
@@ -14,7 +14,7 @@
 namespace Neral
 {
     template <std::size_t Index, std::size_t Length>
-    constexpr char GetChar(const char (&s)[Length])
+    constexpr char GetChar(const char (&s)[Length]) noexcept
     {
         return s[Index < Length ? Index : Length - 1];
     }
@@ -57,26 +57,26 @@ namespace Neral
     }
 
     template <char... Chars>
-    auto AutoEndString(static_string<Chars...>) -> decltype(MakeFixStringOld(static_string<Chars>{}...));
+    auto AutoEndString(static_string<Chars...>) -> decltype(MakeFixString(static_string<Chars>{}...));
     template <char... Previous, char Current, char... After>
-    auto MakeFixStringOld(static_string<Previous...>, static_string<Current>, static_string<After>...)
-        -> decltype(MakeFixStringOld(static_string<Previous..., Current>{}, static_string<After>{}...));
+    auto MakeFixString(static_string<Previous...>, static_string<Current>, static_string<After>...)
+        -> decltype(MakeFixString(static_string<Previous..., Current>{}, static_string<After>{}...));
     template <char... Previous, char... After>
-    auto MakeFixStringOld(static_string<Previous...>, static_string<'\0'>, static_string<After>...)
+    auto MakeFixString(static_string<Previous...>, static_string<'\0'>, static_string<After>...)
         -> static_string<Previous...>;
 
 #define NERAL_MAKE_CHAR_INDEX(n, str) ::Neral::GetChar<0x##n##0>(str), ::Neral::GetChar<0x##n##1>(str), \
-                                     ::Neral::GetChar<0x##n##2>(str), ::Neral::GetChar<0x##n##3>(str), \
-                                     ::Neral::GetChar<0x##n##4>(str), ::Neral::GetChar<0x##n##5>(str), \
-                                     ::Neral::GetChar<0x##n##6>(str), ::Neral::GetChar<0x##n##7>(str), \
-                                     ::Neral::GetChar<0x##n##8>(str), ::Neral::GetChar<0x##n##9>(str), \
-                                     ::Neral::GetChar<0x##n##a>(str), ::Neral::GetChar<0x##n##b>(str), \
-                                     ::Neral::GetChar<0x##n##c>(str), ::Neral::GetChar<0x##n##d>(str), \
-                                     ::Neral::GetChar<0x##n##e>(str), ::Neral::GetChar<0x##n##f>(str)
+                                      ::Neral::GetChar<0x##n##2>(str), ::Neral::GetChar<0x##n##3>(str), \
+                                      ::Neral::GetChar<0x##n##4>(str), ::Neral::GetChar<0x##n##5>(str), \
+                                      ::Neral::GetChar<0x##n##6>(str), ::Neral::GetChar<0x##n##7>(str), \
+                                      ::Neral::GetChar<0x##n##8>(str), ::Neral::GetChar<0x##n##9>(str), \
+                                      ::Neral::GetChar<0x##n##a>(str), ::Neral::GetChar<0x##n##b>(str), \
+                                      ::Neral::GetChar<0x##n##c>(str), ::Neral::GetChar<0x##n##d>(str), \
+                                      ::Neral::GetChar<0x##n##e>(str), ::Neral::GetChar<0x##n##f>(str)
 #define NERAL_MAKE_CHAR_INDEX_64_LOW(str) NERAL_MAKE_CHAR_INDEX(0, str), NERAL_MAKE_CHAR_INDEX(1, str), \
-                                         NERAL_MAKE_CHAR_INDEX(2, str), NERAL_MAKE_CHAR_INDEX(3, str)
+                                          NERAL_MAKE_CHAR_INDEX(2, str), NERAL_MAKE_CHAR_INDEX(3, str)
 #define NERAL_MAKE_CHAR_INDEX_64_HEIGHT(str) NERAL_MAKE_CHAR_INDEX(4, str), NERAL_MAKE_CHAR_INDEX(5, str), \
-                                            NERAL_MAKE_CHAR_INDEX(6, str), NERAL_MAKE_CHAR_INDEX(7, str)
+                                             NERAL_MAKE_CHAR_INDEX(6, str), NERAL_MAKE_CHAR_INDEX(7, str)
 #define NERAL_MAKE_CHAR_INDEX_SEQUENCE(str) NERAL_MAKE_CHAR_INDEX_64_LOW(str), NERAL_MAKE_CHAR_INDEX_64_HEIGHT(str)
 
 #define NERAL_MAKE_STATIC_STRING_TYPE(str) decltype(::Neral::AutoEndString(::Neral::static_string<NERAL_MAKE_CHAR_INDEX_SEQUENCE(str)>{}))
